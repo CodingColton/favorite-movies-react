e7529cefaebd4909d67f4e5e9e6fae79ea584749
CodingColton <100506105+CodingColton@users.noreply.github.com>
2022-09-13 14:07:36 -0500

    Add MovieForm component.

---
+++ movie-list/src/components/MovieForm.jsx
@@ -0,0 +1,11 @@
+export const MovieForm = () => {
+    return (
+        <div>
+            <label htmlFor="title">Movie: </label>
+            <input type="text" />
+            <div>
+                <button>Add to List</button>
+            </div>
+        </div>
+    );
+}
--- movie-list/src/index.js
+++ movie-list/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MovieForm } from './components/MovieForm';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MovieForm />
   </React.StrictMode>
 );
 
