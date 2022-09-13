bfa5d849d3d5eba04a818859c7e2942cae948d4c
CodingColton <100506105+CodingColton@users.noreply.github.com>
2022-09-13 15:11:31 -0500

    Finish list functionality and display with React components.

--- movie-list/src/components/MovieForm.jsx
+++ movie-list/src/components/MovieForm.jsx
@@ -1,20 +1,32 @@
+import { MovieList } from "./MovieList";
+
 export const MovieForm = () => {
-    let favoriteMovies = [];
+    let favoriteMovies = ['Good Movie 1', 'Better Movie: the sequel'];
     
     const AddItem = () => {
         let movieInput = document.getElementById('movie-input').value;
         movieInput !== "" ? favoriteMovies.push(movieInput) : console.log("can't be blank");
         
         document.getElementById('movie-input').value = "";
+        console.log(favoriteMovies);
     };
     
     return (
         <div>
             <label htmlFor="title">Movie: </label>
-            <input type="text" id="movie-input" />
+            <input type="text" id="movie-input" placeholder="Check console for list" />
             <div>
                 <button onClick={AddItem}>Add to List</button>
             </div>
+
+            <div>
+            <h1>Favorite Movies</h1>
+            <ul>
+                {favoriteMovies.map((movie) => (
+                    <MovieList movieName={movie} />
+                ))}
+            </ul>
+        </div>
         </div>
     );
 }
---
+++ movie-list/src/components/MovieList.jsx
@@ -0,0 +1,3 @@
+export const MovieList = ({ movieName }) => {
+    return <li>{ movieName }</li>;
+}
